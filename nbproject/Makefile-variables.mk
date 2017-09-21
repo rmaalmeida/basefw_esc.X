89c8d660dd5ec9b4b2dcd18c07ddf6e3b6b42d73
Rodrigo Almeida <rmaalmeida@gmail.com>
2017-09-20 21:07:23 -0300

    Base firmware for pic18f4550
    This is the base firmware for ESC and DEFCON workshops on embedded
    systems development. All code are done by hand, without using any
    external header or tool.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=basefw_esc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/basefw_esc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=basefwesc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/basefwesc.x.tar
